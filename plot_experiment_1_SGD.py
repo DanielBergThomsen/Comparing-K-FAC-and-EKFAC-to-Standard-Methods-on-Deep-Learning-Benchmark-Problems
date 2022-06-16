7155f018aecebfe5ede00859db1a648f08a87293
DXJ <xianjiedai@gmail.com>
2022-06-16 23:16:22 +0200

    sgd_plot

--- plot_experiment_1_SGD.py
+++ plot_experiment_1_SGD.py
@@ -1,23 +1,3 @@
-'''
-This training file will reproduce part of the first experiment in the original paper of KFAC.
-Two versions of KFAC will be tested for fixed mini-batch size.
-The total number of iterations is 3000, which is given in the original paper.
-Batch size can be selected from 2000, 4000, and 6000
-
-To reproduce the plots in our report, please run this file three times with
-BATCH_SIZE = 2000
-BATCH_SIZE = 4000
-BATCH_SIZE = 6000
-and go to plot_experiment_1.py to plot
-'''
-import torch.optim as optim
-from torch.utils.data import DataLoader
-from pytorch_lightning import Trainer
-from pytorch_lightning.loggers import CSVLogger
-from torchvision.datasets import MNIST
-from torchvision import transforms
-from os.path import isdir
-from autoencoder import VAE
 import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib
@@ -25,16 +5,6 @@ import pandas as pd
 import numpy as np
 import torch
 import logging
-
-#Change batch size for different experiments: 2000, 4000, and 6000
-BATCH_SIZE = 2000
-
-
-'''
-The number of epochs will change according to batch size to make sure that the total number of iterations equals to 3000
-Max epoch = total_number_of_iterations / number_of_iterations_per_epoch
-'''
-MAX_EPOCHS = int(15000/(60000/BATCH_SIZE))
 
 logging.getLogger("pytorch_lightning").setLevel(logging.WARNING)
 NUM_GPUS = 1 if torch.cuda.is_available() else 0
@@ -62,7 +32,6 @@ for config in configs:
     config_name = config['label']
     training_logs = []
 
-    # Load training logs
     train_df = pd.read_csv(f'logs/experiment_1/{config_name}/V0/metrics.csv')
     train_loss = train_df[~train_df.train_loss.isnull()][['train_loss', 'step']]
     train_loss['Optimizer'] = config_name
@@ -71,10 +40,7 @@ for config in configs:
     training_df['train_loss'] = np.log(training_df['train_loss'])
     training_pf.append(training_df)
 
-
-
 matplotlib.use('TKAgg')
-    # Add training losses
 
 final_df = pd.merge(training_pf[0], training_pf[1], how='left', on='step')
 final_df = pd.merge(final_df, training_pf[2], how='left', on='step')
